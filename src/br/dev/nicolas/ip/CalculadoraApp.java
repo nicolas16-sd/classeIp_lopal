8bcb81af3ef96cc76818708a47c630022c87c1d4
NICOLAS DOS SANTOS DURÃO <25132912@CFP127ALUNO.LOCAL>
2025-05-20 08:03:21 -0300

    Projeto finalizado

---
+++ src/br/dev/nicolas/ip/CalculadoraApp.java
@@ -0,0 +1,12 @@
+package br.dev.nicolas.ip;
+
+import br.dev.nicolas.ip.gui.*;
+
+public class CalculadoraApp {
+
+	public static void main(String[] args) {
+		Interface tela = new Interface();
+		
+	}
+
+}
